0d36338c110503711c8b95a2110b2de5f52ec31e
Sifan23 <sifanyg123@gmail.com>
2025-08-31 00:43:23 +0300

    feat:  Users page with DataTable and React Query integration

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client";
 
-const users = () => {
+import React from 'react'
+import { User, columns } from "./columns";
+import { DataTable } from './data-table';
+import { useUsers } from "@/hooks/useUsers";
+
+
+
+const UserPage = () => {
+  const { data, isLoading, isError } = useUsers();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Failed to load users</p>;
   return (
-    <div>users</div>
-  )
+    <div className="">
+      <div className="mb-8 px-4 py-2 bg-secondary rounded-md">
+        <h1 className="font-semibold">All Users</h1>
+      </div>
+
+      <DataTable data={data ?? []} columns={columns}/>
+
+    </div>
+  );
 }
 
-export default users
+export default UserPage
