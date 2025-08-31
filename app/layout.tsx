800e639d63b2872bc45cc66c64539545891cfebf
Sifan23 <sifanyg123@gmail.com>
2025-08-31 23:47:16 +0300

    refactor: title updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Isentry",
+  title: "Isentry Dashboard",
   description: "Isentry assessment dashboard",
   icons: {
     icon: [
